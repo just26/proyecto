e4f819f673ef7ea34580b68aa60bf77c0353bf39
Julio <j.st26@hotmail.com>
2018-03-19 21:11:51 +0100

    Inicial

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id')->unique();
             $table->string('name');
             $table->string('surname')->nullable();
             $table->string('email')->unique();
